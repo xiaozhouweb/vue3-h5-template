30a614bf7ef9474048b62b7bbb4c02cfc460baba
xiaozhoweb <308986121@qq.com>
2022-06-01 16:19:38 +0800

    初始化分支

---
+++ src/router/index.ts
@@ -0,0 +1,20 @@
+
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+const history = createWebHistory()
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/",
+    redirect: "/home",
+  },
+  {
+    path: "/home",
+    name: "home",
+    component: () => import("@/views/index.vue"),
+  },
+];
+const router = createRouter({
+      history,
+      routes
+})
+export default router
+ 
